9a6c1135dfe35c03a2bdd92701fe21f7ed47892a
ikermagolo <74785775+ikermagolo@users.noreply.github.com>
2023-12-06 15:17:53 +0300

    Added the search functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
+
+
+function refreshWeather(response){
+    
+    let tempElement = document.querySelector('#weather-app-temperature');
+    let temperature = response.data.temperature.current; 
+    let cityElement = document.querySelector("#city");
+ 
+    cityElement.innerHTML = response.data.city;
+    tempElement.innerHTML = Math.round(temperature);
+    
+
+}
+
+
+
+function searchCity(city){
+
+    let apikey = "3d814e4ef189oa5f9c0dta1eabf099a2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+    axios.get(apiUrl).then(refreshWeather);
+
+}
+
+
 function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput = document.querySelector('#search-form-input');
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+
+
+searchCity(searchInput.value);
     
 
 }
@@ -10,3 +36,5 @@ cityElement.innerHTML = searchInput.value;
 
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Mwanza");
+ 
