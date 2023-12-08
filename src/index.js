1afdc69def5bced98de55bca1570cbca1f15600c
ikermagolo <74785775+ikermagolo@users.noreply.github.com>
2023-12-08 13:21:19 +0300

    Add APIs functionality and weather icons

--- src/index.js
+++ src/index.js
@@ -5,11 +5,25 @@ function refreshWeather(response){
     let tempElement = document.querySelector('#weather-app-temperature');
     let temperature = response.data.temperature.current; 
     let cityElement = document.querySelector("#city");
- 
+    let humidityElement = document.querySelector('#weather-app-humidity')
+    let windyElement = document.querySelector("#weather-app-wind");
+    let discriptionElement = document.querySelector("#weather-app-description");
+   let iconElement = document.querySelector('img.weather-icon');
+   iconElement.src=response.data.condition.icon_url;
+  
+  
+   
+
+
+
+    discriptionElement.innerHTML = response.data.condition.description;
+    windyElement.innerHTML = response.data.wind.speed;
+    humidityElement.innerHTML =response.data.temperature.humidity;
     cityElement.innerHTML = response.data.city;
     tempElement.innerHTML = Math.round(temperature);
-    
-
+ 
+   console.log(response.data);
+ 
 }
 
 
@@ -36,5 +50,5 @@ searchCity(searchInput.value);
 
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Mwanza");
- 
+searchCity("Dodoma");
+  
